bee2c4ef40d04981e28bd9eab56d4e41479b761c
Daniel Dafoe <dannydafoe@gmail.com>
2018-07-31 17:40:45 -0400

    Test FocusFirst directive to ensure it behaves properly

--- projects/angular-a11y-helpers/src/lib/directives/aah-focus-first.directive.spec.ts
+++ projects/angular-a11y-helpers/src/lib/directives/aah-focus-first.directive.spec.ts
@@ -1,8 +1,28 @@
 import { AahFocusFirstDirective } from './aah-focus-first.directive';
+import { Component, DebugElement } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: `<h2 aahFocusFirst>Focus This First</h2>`
+})
+class TestComponent { }
 
 describe('AahFocusFirstDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AahFocusFirstDirective();
-    expect(directive).toBeTruthy();
+  it('should focus the attached-to element after the view is initialized', () => {
+    // Setup
+    const fixture = TestBed.configureTestingModule({
+      declarations: [AahFocusFirstDirective, TestComponent]
+    })
+    .createComponent(TestComponent);
+
+    const headingEl: HTMLElement = fixture.nativeElement.querySelector('h2');
+    const focus = spyOn(headingEl, 'focus');
+
+    // Detect
+    fixture.detectChanges();
+
+    // Expect
+    expect(focus).toHaveBeenCalled();
   });
 });
